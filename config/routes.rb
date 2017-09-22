64f8203b33fe9cbf95eaa029507a3c7a7a4913ab
Juanse <juanseb@gmail.com>
2017-09-22 05:20:54 +0200

    confirm page done

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,5 @@ Rails.application.routes.draw do
   # JSON with changes
   post '/confirm', to: 'pitches#confirm'
 
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
